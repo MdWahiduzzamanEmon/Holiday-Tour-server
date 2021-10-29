678130550e2384b0708511453fbd358c69207c2d
MdWahiduzzamanEmon <wahedemon09@gmail.com>
2021-10-29 17:24:24 +0600

    update booking

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 var cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
 //middleware 
 app.use(cors());
 app.use(express.json());
@@ -21,16 +22,29 @@ async function run() {
   try {
     await client.connect();
     const database = client.db("Holiday");
-    const tour_place_collection = database.collection("tour_place");
+      const tour_place_collection = database.collection("tour_place");
+      const Booking_info_collection=database.collection("Booking_info");
     
       //get all api
       app.get("/tours", async (req, res) => {
           const result = await tour_place_collection.find({}).toArray();
           res.send(result);
       })
-
-
-
+//get one tour_place 
+      app.get('/tourLocation/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: ObjectId(id) };
+          const result = await tour_place_collection.findOne(query);
+          console.log(result);
+          res.send(result);
+      })
+//post on data 
+      app.post('/bookingInfo', async (req, res) => {
+          console.log(req.body);
+          const result = await Booking_info_collection.insertOne(req.body);
+          console.log(result);
+          res.json(result)
+      })
 
 
 
